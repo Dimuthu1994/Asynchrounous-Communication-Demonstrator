0cde94e3495c5e5df6029c0c928e5d8bbfa03455
Dimuthu1994 <dimuthudiss6@gmail.com>
2021-12-09 11:53:13 +0530

    init

---
+++ README.md
@@ -0,0 +1,64 @@
+# Asynchrounous Communication Demonstrator
+
+# Content
+
+- [Background](#background)
+- [Setup](#setup)
+- [Improvements](#improvements)
+- [Technologies](#technologies)
+- [Libraries](#libraries)
+
+# Background
+
+<img src="https://cdn.shopify.com/s/files/1/2702/8766/files/serial.png?v=1515690348" align="right"
+     alt="Serial Transmission" >
+
+The following test code depicts the communication between a device and a driver using asynchronous and ASCII based serial communication protocols. The application simulates a driver and device. The user can input commands to the driver which would call for actions from the device.
+
+The two text files Rx and Tx acts as the Rx and Tx of the device. The following picture depicts the wiring diagram of the set up.
+
+The device is a scale which sends stable wight to the driver when requested. The driver sends command S\n to get the stable weight monitored from the scale. The scale once the stable weight is calculated sends the data to the driver.
+
+<p align="center">
+  <img src="https://i.ibb.co/N7md6yf/devuce.jpg" alt="Wiring" >
+</p>
+<!-- toc -->
+
+# Setup
+
+- Clone the project cd/ to project file.
+- Run command **npm install** to install all node modules.
+- Open two powershells.
+- Run command **npm run start-device** on one powershell.
+- Run command **npm run start-driver** on the other powershell.
+- You will be able to see that both devices are listening to each other.
+- To initiate a the command to the device type **S** in the terminal of the driver and press **enter**
+- The driver will now send the command **S\n** to the device.
+- Once the device registers the command it will respond back with a weight.
+- You can send the command again in driver as before to check the latest weight.
+
+# Improvements
+
+- I did not have time to include the simulation of the parity bit as an extra checker for broken data.
+- I would like to add the ability for the device or driver to recognize that there is no connection to either.
+- When the scale is sending the data to driver if the driver requests for another weight the device doesnt respond with a busy statement. I would like to invest more time to look at how this can be itegrated.
+- If I had time I would look at standardizing the listening and transmitting function so a common function can be used by both device and driver.
+- Only the command S/n is available in the driver even though the commands send to the device is coded such that a variable command can be easily integrated. I didnt have time to add more functionality and commands.
+- It would be great if I had time to convert speed to baudRate and simulate the baudRate concept.
+- When I change the variable "speed" to values less than 300 the data begins to break as the speed of communication increase. I would like to further analyze the issue and see what improvements can be done.
+- If I had the time I would have created a set of commands and a documentation for users to refer.
+
+# Technologies
+
+<p align="center">
+  <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/es6/es6.png" alt="ES6" >
+</p>
+<p align="center">
+  <img src="https://i.ibb.co/YyKgb2d/download.png" alt="NodeJS" >
+</p>
+
+# Libraries
+
+- **fs** library used for file handling
+- **readline** library used for getting input from user to driver
+
---
+++ driver.js
@@ -0,0 +1,149 @@
+const fs = require("fs");
+const std = require("readline");
+var flag = false;
+var dataArray = [];
+var decString = "";
+var dataString = "";
+var i = 0;
+const speed = 400; //Higher speeds can cause data loss simmilar to (1/baudRate)
+
+//converter for converting string to binary packets
+const converter = (string) => {
+  var buf = Buffer.from(string);
+  var message = [];
+  for (x of buf.values()) {
+    const str = x.toString(2);
+    const array = [...str];
+    var dif = 8 - array.length;
+    for (let i = 0; i < dif; i++) {
+      array.unshift("0");
+    }
+    array.unshift("0");
+    array.push("1");
+    message.push(...array);
+  }
+  return message;
+};
+//Callback function for idle
+const listening = () => {
+  const com2 = std.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  var command2 = "";
+  com2.question("", (reply) => {
+    command2 = reply;
+    console.log(`[DRIVER] Sending Command ${reply} \\n`);
+    com2.close();
+  });
+
+  const idle = setInterval(function () {
+    fs.writeFileSync("Rx.txt", `1`, "binary");
+    if (command2 !== "") {
+      if (command2 === "S") {
+        console.log("[DRIVER] Command Recognized");
+        com2.close();
+        sendSerial(converter(command2));
+        clearInterval(idle);
+      } else {
+        console.log("[DRIVER] Command not Recognized");
+        clearInterval(idle);
+        listening();
+      }
+    }
+  }, speed);
+};
+
+//Call back function to send Command
+const sendSerial = (Com) => {
+  console.log("[DRIVER]  Send Serial");
+  var bit = 0;
+  const sendCom = setInterval(function () {
+    if (bit < 10) {
+      fs.writeFileSync("Rx.txt", `${Com[bit]}`, "binary");
+    } else {
+      clearInterval(sendCom);
+      sendEnd();
+    }
+    bit++;
+  }, speed);
+};
+
+//Callback function to send /n after every command
+const sendEnd = () => {
+  var bit = 0;
+  var endLine = ["0", "0", "0", "0", "0", "1", "0", "1", "0", "1"];
+  const sendCom = setInterval(function () {
+    if (bit < 10) {
+      fs.writeFileSync("Rx.txt", `${endLine[bit]}`, "binary");
+    } else {
+      clearInterval(sendCom);
+      listening();
+    }
+    bit++;
+  }, speed);
+};
+
+//Listening to the TX line of Device
+fs.watchFile(
+  "Tx.txt",
+  {
+    bigint: false,
+    persistent: true,
+    interval: speed,
+  },
+  (curr, prev) => {
+    buf = fs.readFileSync("Tx.txt");
+    decString = buf.toString("ascii");
+    // console.log("Serial Data", decString);
+    //idle State driver keeps sending 1
+    if (decString === "1") {
+      if (!flag) {
+        console.log("[DRIVER] Listening.....");
+      }
+    }
+    //receive initial Start bit 0
+    if (decString === "0") {
+      if (!flag) {
+        flag = true; // set to receive mode
+        console.log("[DRIVER] Receiving Data...");
+      }
+    }
+
+    if (flag) {
+      if (i < 9) {
+        //receiving the 8 bit data String
+        if (i > 0) {
+          dataString = dataString.concat(decString);
+        }
+        i++;
+      }
+      //Check for stop bit 1
+      else if (decString === "1") {
+        var data = parseInt(dataString, 2);
+        dataArray.push(data);
+        console.log("[DRIVER] Received", data);
+        if (data === 10) {
+          const bufData = Buffer.from(dataArray);
+          dataArray = [];
+          console.log(
+            "[DRIVER] Message is: ",
+            bufData,
+            bufData.toString("ascii")
+          );
+        }
+        flag = false; // set to listening mode
+        dataString = "";
+        i = 0;
+      } else {
+        console.log("[DRIVER] Broken Data");
+        flag = false; //set to listening mode
+        dataString = "";
+        i = 0;
+      }
+    }
+  }
+);
+
+//initialize listening
+listening();
